b4bb726942467e7c9f1f6b6923a7f1ad6c2f464c
Jodie Alaine Parker <me@jodiealaine.com>
2014-08-18 21:48:19 +0100

    Change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
 
   devise_for :users
-  root 'pages#home'
+  root 'posts#index'
   get 'about' => 'pages'
 
   # The priority is based upon order of creation: first created -> highest priority.
